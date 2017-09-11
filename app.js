3bae2497c0a344d06df7529476394101b09e632f
Anusone Nanthavongsa <ahnuce@gmail.com>
2017-09-11 16:22:17 -0400

    directives and two way binding

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var myApp = angular.module('myApp', []);
 
 // CONTROLLERS
 //$scope got injected into the controller so now you are able to use the service by just putting the onject in as a parameter
-myApp.controller('mainController', ['$scope', function ($scope) {
-  console.log($scope)
+//the first $scope here is the object that angularjs has created for us to use with built in properties and the second one is the dependency injection
+myApp.controller('mainController', ['$scope', '$timeout', '$filter', function ($scope, $timeout, $filter) {
+
+  $scope.name = 'Anusone';
+  $timeout(function(){
+    $scope.name = 'Everybody';
+  }, 3000);
+$scope.handle = '';
+$scope.lowercasehandle = function(){
+  return $filter('lowercase')($scope.handle);
+};
 }]);
 
+//$scope is the glue that binds the code in the controller to the view
 
 //example of dependency injection
 // var Person = function(firstname, lastname){
@@ -22,3 +32,6 @@ myApp.controller('mainController', ['$scope', function ($scope) {
 //
 // var john = new Person('John', 'Doe');
 // logPerson(john);
+
+
+//directives give directions or instructions for what happens to the dom
